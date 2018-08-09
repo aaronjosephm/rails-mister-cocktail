437e7e4c327b4abf9054787f7a998ca9bcb219d3
Aaron Mednick <aaronjosephm@gmail.com>
2018-08-09 17:47:08 -0400

    Added banner class

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -8,6 +8,7 @@ class DosesController < ApplicationController
   def create
     @dose = Dose.new(dose_params)
     @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    @cocktail = @dose.cocktail
     if @dose.save
       redirect_to cocktail_path(@dose.cocktail)
     else
